e33a6a0e8c42045a579156ebd77c1696bcdf5624
DeadSith <deadsith@hotmail.com>
2016-11-28 11:51:52 +0000

    Moved navbar to viewcomponent

---
+++ src/Identity/ViewComponents/RepoBarViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using Identity.Models.HomeViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.ViewComponents
+{
+    public class RepoBarViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(IRepoView repoView,string activePage="")
+        {
+            if (String.Equals(activePage, "Info"))
+                ViewBag.IsInfoActive = true;
+            else if (String.Equals(activePage, "Repo"))
+                ViewBag.IsFolderViewActive = true;
+            return View(repoView);
+        }
+    }
+}
